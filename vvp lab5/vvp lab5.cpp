558faef681bf705b4a0d69b0a596a3999c0dee21
Валерия <valeria103020@gmail.com>
2020-10-15 16:22:08 +0300

    Добавьте файлы проекта.

---
+++ vvp lab5/vvp lab5.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int A, B, C, AC, BC;
+	printf("Enter A,B,C:\n");
+	scanf_s("%d%d%d", &A, &B, &C);
+	if (((C > A) && (C < B)) || ((C > B) && (C < A)))
+	{
+		AC = C - A;
+		BC = B - C;
+		printf("The product of the segments is equal to %d", AC * BC);
+	}
+	else
+	{
+		printf("Incorrect location of point C");
+	}
+	return 0;
+}
+
